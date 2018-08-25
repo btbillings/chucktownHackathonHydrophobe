b23e726f9ac3052a5482ac44f0e6868f1b221c59
Claudio Spiess <claudiosv@users.noreply.github.com>
2018-08-25 11:21:52 -0400

    Merge

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
-import './App.css';
+import PermanentDrawer from './Main';
 
 class App extends Component {
   render() {
     return (
-      <div className="main-site">
-        <header className="site-header">
-          <h1 className="site-title">Hydrophobe</h1>
-        </header>
-        <Button variant="raised" color="primary">
-          Get Started
-        </Button>
-      </div>
+      <PermanentDrawer />
     );
   }
 }
