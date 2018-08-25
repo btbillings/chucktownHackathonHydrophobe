2332bf3e2304c68cb55636e5f48fd9330eb27b4c
root <billingsbt@g.cofc.edu>
2018-08-25 08:30:55 -0700

    Delete TSX Files

--- src/ToolCard.js
+++ src/ToolCard.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Card, CardContent} from '@material-ui/core';
+import {Card, CardContent, Typography} from '@material-ui/core';
 
 export default class ToolCard extends React.Component {
     constructor(props) {
@@ -16,7 +16,13 @@ export default class ToolCard extends React.Component {
     render() {
         return (
             <Card className="tool-card" >
-                <CardContent >
+                <CardContent>
+                    <Typography variant="title" className="tool-title">
+                        <h1>{this.state.name}</h1>
+                    </Typography>
+                    <Typography variant="body1" className="tool-details">
+                        <p>{this.state.description}</p>
+                    </Typography>
                 </CardContent>
             </Card>
         )
