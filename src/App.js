a44b0ab299db23c2c49d45a209e39963d0c45e2a
Unknown <billingsbt@g.cofc.edu>
2018-08-25 08:03:39 -0700

    Add Component Outline

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="main-site">
+        <header className="site-header">
+          <h1 className="site-title">Hydrophobe</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button variant="raised" color="primary">
+          Get Started
+        </Button>
       </div>
     );
   }
